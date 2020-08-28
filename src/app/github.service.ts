9bf004909b79473646110b404aa8cf6cc6f62288
AmimoG <gilbertmatete6@gmail.com>
2020-08-28 13:49:32 +0300

    Add Files

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 import {User} from './user';
 import {Repos} from './repos';
 import {environment} from '../environments/environment';
